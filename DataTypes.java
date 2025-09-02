12e95bc9e4714eefda3e22cad5577c0ad20dd1f7
pritamcse2014 <pritam.cse2014@gmail.com>
2025-09-02 23:23:34 +0600

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+        float f = 5.8f;
+        char c = 'k';
+        boolean b = true;
+    }
+}
