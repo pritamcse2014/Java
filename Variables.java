00442b0e988b3bd2c46717c950ef57d9c7c5d9cd
pritamcse2014 <pritam.cse2014@gmail.com>
2025-09-02 23:05:51 +0600

    Variables

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println(5 + 5);
+        int num1 = 15;
+        int num2 = 10;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
