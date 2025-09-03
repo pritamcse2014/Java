28074df6b401702569ff16f499998549bb91cc63
pritamcse2014 <pritam.cse2014@gmail.com>
2025-09-03 23:25:30 +0600

    RelationalOperators

---
+++ RelationalOperators.java
@@ -0,0 +1,33 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        boolean result1 = x < y;
+        boolean result2 = x > y;
+        boolean result3 = x <= y;
+        boolean result4 = x >= y;
+        boolean result5 = x != y;
+        boolean result6 = x == y;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+
+        double a = 6.5;
+        double b = 5.4;
+        boolean result7 = a < b;
+        boolean result8 = a > b;
+        boolean result9 = a <=b;
+        boolean result10 = a >= b;
+        boolean result11 = a != b;
+        boolean result12 = a == b;
+        System.out.println(result7);
+        System.out.println(result8);
+        System.out.println(result9);
+        System.out.println(result10);
+        System.out.println(result11);
+        System.out.println(result12);
+    }
+}
