ed1d2357087fb4188b1530b7fdab9f40045e9012
pritamcse2014 <pritam.cse2014@gmail.com>
2025-09-06 12:34:06 +0600

    DoWhileLoop

---
+++ DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i + " Hi ");
+            i++;
+        } while (i <= 10);
+    }
+}
